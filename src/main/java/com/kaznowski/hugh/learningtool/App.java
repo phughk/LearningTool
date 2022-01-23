f2e79dfd967e39926dc3a1bb7751c5c7df5d625a
Hugh Kaznowski <phkaznowski@gmail.com>
2022-01-23 16:27:43 +0000

    Initialise project

---
+++ src/main/java/com/kaznowski/hugh/learningtool/App.java
@@ -0,0 +1,13 @@
+package com.kaznowski.hugh.learningtool;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
